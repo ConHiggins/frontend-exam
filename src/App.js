686eb501c201e423da8ba8f0097a1911fca378ee
Conor Higgins <wodowods@gmail.com>
2023-01-20 15:46:46 +0000

    Created temorary JSON version of data / Initialised component & container files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-import gamesData from "./data/data sample.csv";
+import gamesData from "./data/data sample.json";
+import csvToArrayOfObjects from "./utils/csvToArrayOfObjects";
 
 function App() {
+    console.log(gamesData);
     return <div className="App"></div>;
 }
 
---
+++ src/utils/csvToArrayOfObjects.js
@@ -0,0 +1,5 @@
+const csvToArrayOfObjects = (csvData) => {
+    ///TODO: create function to convert CSV to object array
+};
+
+export default csvToArrayOfObjects;
